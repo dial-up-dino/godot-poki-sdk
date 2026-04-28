8e4515848f7a7a963d9aee40547bc6ea3ea926e1
Yngvarr <k.yngvarr@gmail.com>
2026-04-28 15:43:34 +0400

    gameplay control

--- addons/poki-sdk/plugin.gd
+++ addons/poki-sdk/plugin.gd
@@ -2,19 +2,20 @@
 extends EditorPlugin
 
 
-func is_poki_added(cfg:ConfigFile):
+func is_poki_added(cfg: ConfigFile):
 	var pokiexists = false
 	for key in cfg.get_sections():
 		if cfg.has_section_key(key, "name"):
-			var name:String = cfg.get_value(key, "name")
+			var name: String = cfg.get_value(key, "name")
 			if name.begins_with("Poki"):
 				pokiexists = true
 				break
 	return pokiexists
 
-func add_poki_export(cfg:ConfigFile):
+
+func add_poki_export(cfg: ConfigFile):
 	var arr = cfg.get_sections()
-	var num_exports = len(arr)/2
+	var num_exports = len(arr) / 2
 	var poki_section = "preset" + "." + str(num_exports)
 	var poki_options = poki_section + ".options"
 
@@ -22,7 +23,7 @@ func add_poki_export(cfg:ConfigFile):
 	cfg.set_value(poki_section, "name", "Poki")
 	cfg.set_value(poki_section, "platform", "Web")
 	cfg.set_value(poki_section, "runnable", false)
-	cfg.set_value(poki_section, "custom_features", "poki")
+	cfg.set_value(poki_section, "custom_features", "")
 	cfg.set_value(poki_section, "export_filter", "all_resources")
 	cfg.set_value(poki_section, "include_filter", "")
 	cfg.set_value(poki_section, "exclude_filter", "")
@@ -30,11 +31,18 @@ func add_poki_export(cfg:ConfigFile):
 
 	#preset.x.options
 	cfg.set_value(poki_options, "html/custom_html_shell", "res://addons/poki-sdk/full-size.html")
-	cfg.set_value(poki_options, "html/head_include", """
+	(
+		cfg
+		. set_value(
+			poki_options,
+			"html/head_include",
+			"""
 	<script>
 		//place sitelock code here.
 	</script>
-	""")
+	"""
+		)
+	)
 	cfg.set_value(poki_options, "html/focus_canvas_on_start", true)
 	cfg.set_value(poki_options, "html/experimental_virtual_keyboard", false)
 
@@ -43,7 +51,7 @@ func _enter_tree():
 	var cfg = ConfigFile.new()
 	cfg.load("res://export_presets.cfg")
 
-	if(self.is_poki_added(cfg)):
+	if self.is_poki_added(cfg):
 		print("Poki export already added")
 	else:
 		add_poki_export(cfg)
--- addons/poki-sdk/pokisdk.gd
+++ addons/poki-sdk/pokisdk.gd
@@ -1,15 +1,18 @@
 extends Node
 
-var sdk_handle = null
-var _cb_commercial_break
-var _cb_reward_break
-var _cb_shareable_url
-
 signal ad_started
 signal ad_ended
 signal commercial_break_ended(response)
 signal rewarded_break_ended(success: bool)
 signal shareable_url_ready(url)
+
+const VERBOSE := false
+
+var sdk_handle = null
+var _cb_commercial_break
+var _cb_reward_break
+var _cb_shareable_url
+var is_gameplay_started := false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,44 +20,55 @@ func _ready():
 	if not OS.has_feature("poki"):
 		return
 
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	sdk_handle = JavaScriptBridge.get_interface("PokiSDK")
 	_cb_commercial_break = JavaScriptBridge.create_callback(Callable(self, "on_commercial_break"))
 	_cb_reward_break = JavaScriptBridge.create_callback(Callable(self, "on_reward_break"))
 	_cb_shareable_url = JavaScriptBridge.create_callback(Callable(self, "on_shareable_url"))
 
 
-func gameplay_start():
+func gameplayStart():
+	if is_gameplay_started:
+		if VERBOSE:
+			print("gameplayStart is called while gameplay is already started")
+		return
+
+	if OS.has_feature("poki"):
+		if sdk_handle:
+			sdk_handle.gameplayStart()
+		else:
+			print("PokiSDK is not initialized, no gameplay started")
+	else:
+		print_rich("simulating [b][color=green]gameplayStart()[/color][/b]")
+
+	is_gameplay_started = true
+
+
+func gameplayStop():
+	if not is_gameplay_started:
+		if VERBOSE:
+			print("gameplayStop is called while gameplay is not started")
+		return
+
+	if OS.has_feature("poki"):
+		if sdk_handle:
+			sdk_handle.gameplayStop()
+		else:
+			print("PokiSDK is not initialized, no gameplay stopped")
+	else:
+		print_rich("simulating [b][color=green]gameplayStop()[/color][/b]")
+
+	is_gameplay_started = false
+
+
+func commercialBreak():
 	if not OS.has_feature("poki"):
-		print("simulating gameplayStart()")
-		return
-
-	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no gameplay started")
-		return
-
-	sdk_handle.gameplayStart()
-
-
-func gameplay_stop():
-	if not OS.has_feature("poki"):
-		print("simulating gameplayStop()")
-		return
-
-	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no gameplay stopped")
-		return
-
-	sdk_handle.gameplayStop()
-
-
-func commercial_break():
-	if not OS.has_feature("poki"):
-		print("simulating commercialBreak()")
+		print_rich("simulating [b][color=yellow]commercialBreak()[/color][/b]")
 		on_commercial_break({})
 		return
 
 	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no commercial break")
+		print("PokiSDK is not initialized, no commercial break")
 		return
 
 	ad_started.emit()
@@ -63,19 +77,19 @@ func commercial_break():
 
 func on_commercial_break(args):
 	print("Commercial break done!")
-	print(args)
+	# print(args)
 	ad_ended.emit()
 	commercial_break_ended.emit(args)
 
 
-func rewarded_break():
+func rewardedBreak():
 	if not OS.has_feature("poki"):
-		print("simulating rewardedBreak()")
+		print_rich("simulating [b][color=yellow]rewardedBreak()[/color][/b]")
 		on_reward_break([true])
 		return
 
 	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no rewarded break")
+		print("PokiSDK is not initialized, no rewarded break")
 		return
 
 	ad_started.emit()
@@ -88,9 +102,9 @@ func on_reward_break(args: Array):
 	rewarded_break_ended.emit(args[0])
 
 
-func shareable_url(obj: Dictionary):
+func shareableURL(obj: Dictionary):
 	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no shareable URL")
+		print("PokiSDK is not initialized, no shareable URL")
 		return
 
 	var params = JavaScriptBridge.create_object("Object")
@@ -105,9 +119,9 @@ func on_shareable_url(url):
 	shareable_url_ready.emit(url)
 
 
-func is_ad_blocked():
+func isAdBlocked():
 	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no add blocking check")
+		print("PokiSDK is not initialized, no add blocking check")
 		return
 
 	var ret = sdk_handle.isAdBlocked()
@@ -116,7 +130,7 @@ func is_ad_blocked():
 
 func enableEventTracking():
 	if not sdk_handle:
-		push_warning("PokiSDK is not initialized, no event tracking")
+		print("PokiSDK is not initialized, no event tracking")
 		return
 
 	sdk_handle.enableEventTracking()
